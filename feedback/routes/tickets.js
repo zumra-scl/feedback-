a13e6cea457be88d676b90c008ba2987adb0ae81
Zumra <s2401127@edu.bc.fi>
2026-05-23 01:42:37 +0300

    osa 3

--- feedback/routes/tickets.js
+++ feedback/routes/tickets.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const db = require("../db/db");
 
+const statusMap = {
+  open: 1,
+  "working on": 2,
+  done: 3,
+  closed: 4,
+};
+
 router.get("/", async (req, res) => {
   const [rows] = await db.query(`
     SELECT
@@ -34,7 +41,8 @@ router.get("/view", async (req, res) => {
       support_ticket.arrived,
       customer.name AS customer,
       support_ticket.description,
-      ticket_status.description AS status
+      ticket_status.description AS status,
+      support_ticket.handled
     FROM support_ticket
     JOIN customer
       ON support_ticket.customer_id = customer.id
@@ -75,6 +83,34 @@ router.post("/reply", async (req, res) => {
   `,
     [ticket_id, 1, message],
   );
+
+  res.redirect("/tickets/view?id=" + ticket_id);
+});
+
+router.post("/status", async (req, res) => {
+  const { ticket_id, status } = req.body;
+
+  const statusId = statusMap[status];
+
+  await db.query(
+    `
+    UPDATE support_ticket
+    SET status = ?
+    WHERE id = ?
+  `,
+    [statusId, ticket_id],
+  );
+
+  if (status === "closed") {
+    await db.query(
+      `
+      UPDATE support_ticket
+      SET handled = NOW()
+      WHERE id = ?
+    `,
+      [ticket_id],
+    );
+  }
 
   res.redirect("/tickets/view?id=" + ticket_id);
 });
